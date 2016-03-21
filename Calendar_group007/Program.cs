d03faa6dce25602f5dd5ab8ee59ad7bfdab8f05a
peymantp <peymantparsa@gmail.com>
2016-03-21 16:47:56 -0700

    program locked to one instance

--- Calendar_group007/Program.cs
+++ Calendar_group007/Program.cs
@@ -9,11 +9,20 @@ namespace PJCalender
 {
     static class Program
     {
+        static Mutex mutex = new Mutex(true, "{8F6F0AC4-B9A1-45fd-A8CF-72F04E6BDE8F}");
+        [STAThread]
         static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Menus());
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Menus());
+                mutex.ReleaseMutex();
+            }
+            else {
+                MessageBox.Show("only one instance at a time");
+            }
         }
         public static void Exit()
         {
