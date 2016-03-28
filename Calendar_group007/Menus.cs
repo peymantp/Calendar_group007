8fe342c870f945e629ee86c7a9712bf9fdda7bc4
peymantp <peymantparsa@gmail.com>
2016-03-28 13:44:38 -0700

    made gloabal values for Menus.cs
    DateTime Selected for the day selected by user
    ArrayList events for all the events for the user(will probably change with database)

--- Calendar_group007/Menus.cs
+++ Calendar_group007/Menus.cs
@@ -10,13 +10,24 @@ namespace PJCalender
     /// </summary>
     public partial class Menus : Form
     {
+        private DateTime Selected
+        {
+            get;
+            set;
+        }
+
+        public System.Collections.ArrayList events {
+            get;
+            set;
+        }
+
         public Menus()
         {
             InitializeComponent();
             loginButtonChangeText();
-            dateTimePicker.Value = DateTime.Now;
-            labelDay.Text = DateTime.Now.ToLongDateString();
-                
+            Selected = DateTime.Now;
+            dateTimePicker.Value = Selected;
+            displayAll();
         }
         /// <summary>
         /// Exits the program
@@ -44,48 +55,10 @@ namespace PJCalender
         /// <param name="e">Event</param>
         private void dateTimePicker_DatePicked(object sender, EventArgs e)
         {
-            DateTime t = dateTimePicker.Value;
-            labelDay.Text = t.ToLongDateString();
-            monthDayNumber(t);
+            Selected = dateTimePicker.Value;
+            displayAll();
         }
-        /// <summary>
-        /// Returns the number of days in a month sent to the function
-        /// </summary>
-        /// <param name="date">A date containing a month</param>
-        /// <returns>number of days in a month</returns>
-        private int monthDayNumber(DateTime date)
-        {
-            switch (date.Month)
-            {
-                case 1: //January
-                    return 31;
-                case 2: // February
-                    if (DateTime.IsLeapYear(date.Year))
-                        return 29;
-                    return 28;
-                case 3:     //March
-                    return 31;
-                case 4:     //April
-                    return 30;
-                case 5:     //May
-                    return 31;
-                case 6:     //June
-                    return 30;
-                case 7:     //July
-                    return 31;
-                case 8:     //August
-                    return 31;
-                case 9:     //September
-                    return 30;
-                case 10:    //October
-                    return 31;
-                case 11:    //November
-                    return 30;
-                case 12:    //December
-                    return 31;
-            }
-            return 0;
-        }
+       
         /// <summary>
         /// Changes the text on the button login/logout if a new user is created
         /// </summary>
